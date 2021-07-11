42fe7dad785446ed2560f1ed00f5c92430f30d6a
ShaySha-PRa <shaycormac@foxmail.com>
2021-07-11 17:31:45 +0800

    my code without style and only simple index.html

---
+++ online_editor/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from . import views
+
+app_name = 'online_editor'
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'execute$', views.execute, name='execute'),
+]
--- online_editor/views.py
+++ online_editor/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.conf import settings
+from . core import run_in_docker
 
-# Create your views here.
+def index(request):
+    context = {}
+    return render(request, 'index.html', context)
+
+def execute(request):
+    code = request.body.decode() + "\n"
+    
+    input_name = settings.INPUT_FILE_PATH
+    output_name = settings.OUTPUT_FILE_PATH
+    shell_script = settings.RUN_IN_DOCKER_SH_PATH
+    
+    # run in docker
+    result = run_in_docker(input_name, code, shell_script, output_name)
+    # enable pretty html formatting and obfuscate input file name with 'source'
+    result = result.replace('\n', "<br>").replace(input_name, "source")
+
+    return JsonResponse({'result':result})

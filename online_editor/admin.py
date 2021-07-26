05d2cc47e6f825e6f7b958d5db5abd81f5dcac7f
kuhat <1043744621@qq.com>
2021-07-26 22:40:55 +0800

    First Deployment

--- online_editor/admin.py
+++ online_editor/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Codes
+
+
 # Register models
 class CodesAdmin(admin.ModelAdmin):
     list_display = ['code_id',
@@ -7,4 +9,6 @@ class CodesAdmin(admin.ModelAdmin):
                     'compile_status',
                     'run_status_time',
                     'run_status_memory']
+
+
 admin.site.register(Codes, CodesAdmin)

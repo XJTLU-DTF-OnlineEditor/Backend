df2028b3d6ba51fc1081910e4472985b467bbffb
Your Name <you@example.com>
2021-08-11 23:47:33 +0800

    The run interface was submitted for the third time, and interactive functions were added. The interactive interface needed to be tested on the front end.

--- online_editor/views.py
+++ online_editor/views.py
@@ -3,7 +3,7 @@
     add the section of the compile
 
 """
-#INSERT INTO online_editor_codes (code_id, code_result,compile_status) VALUES (1,"shuchu","true");
+# INSERT INTO online_editor_codes (code_id, code_result,compile_status) VALUES (1,"shuchu","true");
 from django.db.models import Model
 from django.http.response import HttpResponseForbidden
 from django.shortcuts import render
@@ -25,14 +25,15 @@ def run(request):
     output_name = settings.OUTPUT_FILE_PATH
     shell_script = settings.RUN_IN_DOCKER_SH_PATH
     try:
-        request_content = json.loads(request.body)
+        body_unicode = request.body.decode() + "\n"
+        request_content = json.loads(body_unicode)
         lang = request_content.get("lang")
+        input_type = request_content.get("type")
         memory_limit = request_content.get("memory_limit")
         input = request_content.get("input")
         source = request_content.get("source")
-        source_check(source)#检查source是否为空
         time_limit = request_content.get("time_limit")
-        compile_status = request_content.get("compile_status")
+        # compile_status = request_content.get("compile_status")
         id = request_content.get("id")
     except TimeoutError:
         response = {
@@ -46,42 +47,40 @@ def run(request):
             "msg": "Server does not understand the requested syntax"
         }
         return JsonResponse(response, safe=False)
-
     errors = ""
     result = ""
     error_code = 200
     msg = "success"
     # 如果没有编译
-    if not compile_status:
+    if input_type == "Split" or input_type == "Interactive":
         try:  # 跑代码
-            result = run_in_docker(input_name, source, shell_script, output_name)
+            result = run_in_docker(input_name, source, shell_script, output_name, input, input_type)
             # enable pretty html formatting and obfuscate input file name with 'source'
-            result = result.replace('\n', "<br>").replace(input_name, "source")
+        except ValueError as r:
+            error_code = 501
+            msg = " (Not yet implemented) Server has not implemented the function, Please check your input."
+            errors = "运行时出现错误: %s" % r
+
         except Exception as r:  # 处理运行时的错误并将错误存入数据库
             error_code = 500
             msg = " Server has encountered error, cannot resolve request"
             errors = "运行时出现错误: %s" % r
         finally:  # 将数据存入数据库
-
+            result = result.replace(input_name, "source")
             code_response = Codes.objects.create(  # 存入数据库
                 code_id=id,
                 code_content=source,
-                compile_status=compile_status,
+                compile_status=True,
                 errors=errors,
                 code_result=result,
             )
             code_response.save()
-
-
-    else:  # 如果已经编译,代表数据库中有该代码和他的输出
-        try:  # 从数据库中寻找code
-            code_ob = Codes.objects.get(code_id=id)
-
-        except Exception as e:  # 查找错误则抛出异常
-            errors = "数据库错误：%s" % e
-            result = ""
-        else:  # 没有问题则将数据库中的result赋值
-            result = code_ob.code_result
+    else:
+        response = {
+            "error_code": 400,
+            "msg": "Server does not understand the requested syntax"
+        }
+        return JsonResponse(response, safe=False)
 
     # 最后运行成功返回json数据
     run_data_backend = {
@@ -91,20 +90,12 @@ def run(request):
             'request_status': "success",
             'errors': errors,
             'time_limit': time_limit,
-            'compile_status': compile_status,
+            'compile_status': True,
             'run_status': "OK",
             'Output': result,
             'id': id, }
     }
     return JsonResponse(run_data_backend)
-
-
-def source_check(source):
-    if source == "":
-        response = {
-            "message": "Source can't be empty!"
-        }
-        return JsonResponse(response, safe=False)
 
 
 def missing_argument_error():

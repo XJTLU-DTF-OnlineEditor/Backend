355666e837b2283bd6b10733f843d226f2e42e70
kuhat <1043744621@qq.com>
2022-02-25 15:45:23 +0800

    Add databases mock data

--- Django_editor_backend/settings.py
+++ Django_editor_backend/settings.py
@@ -90,12 +90,12 @@ WSGI_APPLICATION = 'Django_editor_backend.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 # if DEBUG == True:
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         }
 #     }
-# }
 # else:
 DATABASES = {
     'default': {

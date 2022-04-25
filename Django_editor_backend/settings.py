3d764751d72948bce2f8af04ae509e13dc80ef97
Danny Hao <53931269+kuhat@users.noreply.github.com>
2022-04-25 11:36:34 +0800

    Update settings.py

--- Django_editor_backend/settings.py
+++ Django_editor_backend/settings.py
@@ -110,7 +110,7 @@ CHANNEL_LAYERS = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'online_editor',
+        'NAME': 'online_editor1',
         'HOST': '47.111.13.213',
         'POST': 3306,
         'USER': 'root',
@@ -171,7 +171,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True  # 是否使用TLS安全传输协议(用于在两个通信应用程序之间提供保密性和数据完整性。)
 EMAIL_USE_SSL = False  # 是否使用SSL加密，qq企业邮箱要求使用
 EMAIL_HOST = 'smtp.163.com'  # 发送邮件的邮箱 的 SMTP服务器，这里用了163邮箱
-EMAIL_PORT = 25  # 发件箱的SMTP服务器端口，或者 995是设置了 SSL 加密方式
+EMAIL_PORT = 465  # 发件箱的SMTP服务器端口，或者 995是设置了 SSL 加密方式
 EMAIL_HOST_USER = 'xjtluonline_editor@163.com'  # 发送邮件的邮箱地址
 EMAIL_HOST_PASSWORD = 'GAWCDWZBUEBDEMIR'  # 发送邮件的邮箱密码(这里使用的是授权码)
 EMAIL_FROM = "XJTLU_Online_editor<xjtluonline_editor@163.com>"
